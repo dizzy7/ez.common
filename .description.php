6f05bfdba54a577fd7503a6976769961c65060dc
Dizzy <7dizzy7@gmail.com>
2014-02-13 15:14:17 +0400

    Добавлены настройки инфоблока

---
+++ .description.php
@@ -0,0 +1,19 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => "Карта объектов",
+	"DESCRIPTION" => "Карта объектов(магазинов) из инфоблока (яндекс-карты)",
+	"ICON" => "/images/map_view.gif",
+	"SORT" => 20,
+	"SCREENSHOT" => array(
+		"/images/shopmap.png",
+	),
+	"CACHE_PATH" => "Y",
+	"PATH" => array(
+		"ID" => "mediasfera",
+        "NAME" => "Медиасфера"
+	),
+);
+
+?>
