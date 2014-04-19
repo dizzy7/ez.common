59672f7631f27257f345ec1cba51f5b78b98ab0a
Dizzy <dizzy@localhost.localdomain>
2014-04-19 22:00:11 +0400

    .gitignore update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1f508c8a5c6459e3003426a98d6670a7::getLoader();
+return ComposerAutoloaderInit8e4650dcf32bf807314676cd69168bf0::getLoader();
